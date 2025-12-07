e55fc21e31b01f6e9fe17858b65f661fc9f67596
spoonmilk <haku31229@gmail.com>
2025-12-07 01:48:41 -0500

    [feat] Add GenericRunner Trait

---
+++ src/algorithms/generic_runner.rs
@@ -0,0 +1,120 @@
+use std::collections::HashMap;
+
+use crate::bpf::DatapathEvent;
+use ebpf_ccp_cubic::{GenericAlgorithm, kenericFlow};
+use tracing::warn;
+
+use crate::algorithms::AlgorithmRunner;
+
+pub struct GenericRunner<A: GenericAlgorithm> {
+    algorithm: A,
+    flows: HashMap<u64, Box<dyn GenericFlow>>,
+    init_cwd: u64,
+    mss: u32,
+}
+
+impl<A: GenericAlgorithm> GenericRunner<A> {
+    pub fn new(algorithm: A, init_cwnd: u32, mss: u32) -> Self {
+        Self {
+            algorithm: A,
+            flows: HashMap::new(),
+            init_cwd: init_cwd,
+            mss: mss,
+        }
+    }
+}
+
+impl<A: GenericAlgorithm> AlgorithmRunner for GenericRunner<A> {
+    fn name(&self) -> &str {
+        self.algorithm.name()
+    }
+
+    fn ebpf_path(&self) {
+        "ebpf/.output/generic.bpf.o"
+    }
+
+    fn struct_ops_name(&self) -> &str {
+        "ebpf_ccp_generic"
+    }
+
+    fn handle_event(
+        &mut self,
+        event: crate::bpf::DatapathEvent,
+    ) -> anyhow::Result<Option<super::CwndUpdate>> {
+        match event {
+            DatapathEvent::FlowCreated {
+                flow_id,
+                init_cwnd,
+                mss,
+            } => {
+                let new_flow = self.algorithm.create_flow(init_cwnd, mss);
+                self.flows.insert(flow_id, GenericFlow { new_flow, mss })
+            }
+
+            DatapathEvent::Measurement {
+                flow_id,
+                measurement,
+            } => {
+                let flow = self
+                    .flows
+                    .get_mut(&flow_id)
+                    .ok_or_else(|| anyhow!("Unknown flow"))?;
+
+                // Convert Measurement → Report
+                let report = Report {
+                    flow_key: measurement.flow,
+
+                    // Flow statistics
+                    packets_in_flight: measurement.flow_stats.packets_in_flight,
+                    bytes_in_flight: measurement.flow_stats.bytes_in_flight,
+                    bytes_pending: measurement.flow_stats.bytes_pending,
+                    rtt_sample_us: measurement.flow_stats.rtt_sample_us,
+                    was_timeout: measurement.flow_stats.was_timeout != 0,
+
+                    // ACK statistics
+                    bytes_acked: measurement.ack_stats.bytes_acked,
+                    packets_acked: measurement.ack_stats.packets_acked,
+                    bytes_misordered: measurement.ack_stats.bytes_misordered,
+                    packets_misordered: measurement.ack_stats.packets_misordered,
+                    ecn_bytes: measurement.ack_stats.ecn_bytes,
+                    ecn_packets: measurement.ack_stats.ecn_packets,
+                    lost_pkts_sample: measurement.ack_stats.lost_pkts_sample,
+
+                    // Kernel context
+                    snd_cwnd: measurement.snd_cwnd,
+                    snd_ssthresh: measurement.snd_ssthresh,
+                    pacing_rate: measurement.pacing_rate,
+                    ca_state: measurement.ca_state,
+                    now: measurement.ack_stats.now,
+
+                    rate_incoming: 0,
+                    rate_outgoing: 0,
+                };
+
+                if report.was_timeout {
+                    flow.reset();
+                } else if report.lost_pkts_sample > 0 {
+                    flow.reduction(&report);
+                } else if report.bytes_acked > 0 {
+                    flow.increase(&report);
+                }
+
+                // Return cwnd update
+                Ok(Some(CwndUpdate {
+                    flow_id,
+                    cwnd_bytes: flow.curr_cwnd(),
+                }))
+            }
+
+            DatapathEvent::FlowClosed { flow_id } => {
+                self.flows.remove(&flow_id);
+                Ok(None)
+            }
+        }
+    }
+
+    fn cleanup(&mut self) {
+        info!("Cleaning up {} datapath flows", self.flows.len());
+        self.flows.clear();
+    }
+}
